b0d37992fac5d588a08053cdfcaa69f5b73401f4
leizhang <Lei_Zhang-w2i@ruhr-uni-bochum.de>
2024-10-29 07:48:15 +0100

    major change: 1. centralize parameters that used more than once in config. 2. make sure every snakefile can work and have the same result as in paper.

---
+++ digital_discovery_work_flow/prediction_models/script/add_predicted_value.py
@@ -0,0 +1,16 @@
+# add_predicted_value.py
+import pandas as pd
+import argparse
+
+def add_predicted_value(input_file, source_column):
+    df = pd.read_csv(input_file)
+    df['Predicted_Value'] = df[source_column]
+    df.to_csv(input_file, index=False)  # Overwriting the original file
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Add a Predicted_Value column to CSV files.")
+    parser.add_argument("--input_file", required=True, help="Input CSV file")
+    parser.add_argument("--source_column", required=True, help="Source column to copy data from")
+    args = parser.parse_args()
+
+    add_predicted_value(args.input_file, args.source_column)
