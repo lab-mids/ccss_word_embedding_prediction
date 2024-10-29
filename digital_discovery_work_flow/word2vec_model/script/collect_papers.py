d7276446fb3bb73b24b7bf10ef079e7442ed733a
leizhang <Lei_Zhang-w2i@ruhr-uni-bochum.de>
2024-10-29 18:01:23 +0100

    update the collect paper process

--- digital_discovery_work_flow/word2vec_model/script/collect_papers.py
+++ digital_discovery_work_flow/word2vec_model/script/collect_papers.py
@@ -1,7 +1,6 @@
 import argparse
 from matnexus import PaperCollector as spc
 
-
 def parse_args():
     parser = argparse.ArgumentParser(description="Collect papers from multiple sources")
     parser.add_argument(
